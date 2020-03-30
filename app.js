cedd4b328f5c71245a84c7ff3310bff7b3354b20
Piyush <piyush.kumardav@gmail.com>
2020-03-30 15:13:12 +0530

    Second commit

---
+++ app.js
@@ -0,0 +1,9 @@
+express = require('express')
+var app = express();
+app.set('view engine','jade');
+app.get('/',function(req,res)
+{
+                res.render('index');
+});
+var server = app.listen(3000,function(){});
+
