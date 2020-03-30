2a9ad2b44d02c439c8ca2538d8e0b3f241100373
Piyush <piyush.kumardav@gmail.com>
2020-03-30 21:03:57 +0530

    food

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ app.get('/',function(req,res)
 {
                 res.render('index');
 });
+app.get('/food',function(req,res)
+{
+        res.render('restaurant.jade');
+})
 var server = app.listen(3000,function(){});
 
